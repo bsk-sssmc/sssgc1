f4211737612710163826cb41d8568c8dfbe4bd94
Saikrishna <saikrishna@sssmediacentre.org>
2023-10-20 10:40:29 +0530

    Commit About Us page

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { TiThMenu } from "react-icons/ti";
 
 export default function Navbar() {
@@ -15,10 +16,27 @@ export default function Navbar() {
         </div>
         <div className="flex flex-row gap-3 align-middle">
           <div className="hover:text-slate-300 hover: px-3">
-            <h2>Sri Sathya Sai</h2>
+            <Link
+              href={`/sri-sathya-sai-baba`}
+              as={`/sri-sathya-sai-baba`}
+              passHref
+            >
+              <h2>Sri Sathya Sai</h2>
+            </Link>
+          </div>
+          <div className="hover:text-slate-300 hover: px-3">
+            <Link
+              href={`/prasanthi-nilayam`}
+              as={`/prasanthi-nilayam`}
+              passHref
+            >
+              <h2>Prasanthi Nilayam</h2>
+            </Link>
           </div>
           <div className="hover:text-slate-300 px-3">
-            <h2>About Us</h2>
+            <Link href={`/about-us`} as={`/about-us`} passHref>
+              <h2>About Us</h2>
+            </Link>
           </div>
           <div className="hover:text-slate-300 px-3">
             <h2>Community Engagement</h2>
---
+++ pages/about-us.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/common/Navbar";
+import Footer from "@/components/common/Footer";
+import About from "@/components/About";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function AboutUs() {
+  return (
+    <>
+      <div className="top-0 bg-blue-900 p-4 font-bold text-base navbar-main">
+        <Navbar />
+      </div>
+
+      <About />
+
+      <div className="flex bg-blue-900 text-white footer justify-center">
+        <div className="relative bottom-0  w-[50vw] justify-center p-4 font-bold text-base navbar-main">
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+}
